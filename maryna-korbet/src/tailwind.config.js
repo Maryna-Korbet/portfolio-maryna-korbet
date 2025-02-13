ab0132f4c9112ebf2ac7498ef1cd5c2c758cc0dc
Maryna-Korbet <m22v86k@gmail.com>
2025-02-12 23:37:11 -0500

    add tailwindScrollbar

--- maryna-korbet/src/tailwind.config.js
+++ maryna-korbet/src/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
     content: [
         "./index.html",
@@ -12,5 +14,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [tailwindScrollbar],
 }
